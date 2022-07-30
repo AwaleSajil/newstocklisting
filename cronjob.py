ccd475c9920076d300d03933879f948bd110cf61
Sajil Awale <sajil.awale@CGNPLAP271.local>
2022-07-30 21:33:05 +0545

    telegram

--- cronjob.py
+++ cronjob.py
@@ -6,6 +6,6 @@ from notify_new_listing import schedule_job
 
 # Create an instance of scheduler and add function.
 scheduler = BlockingScheduler()
-scheduler.add_job(schedule_job, "interval", seconds=120)
+scheduler.add_job(schedule_job, "interval", seconds=1800)
 
 scheduler.start()
