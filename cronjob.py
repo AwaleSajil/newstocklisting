f48ba64f37b82bb541c5d1f14954c09f2fe3bd2b
Sajil Awale <sajil.awale@CGNPLAP271.local>
2022-07-31 00:25:30 +0545

    flask3

--- cronjob.py
+++ cronjob.py
@@ -5,7 +5,10 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from notify_new_listing import schedule_job
 
 # Create an instance of scheduler and add function.
-scheduler = BlockingScheduler()
-scheduler.add_job(schedule_job, "interval", seconds=120)
+scheduler = BlockingScheduler(timezone="Asia/Kathmandu")
+
+
+scheduler.add_job(schedule_job, "cron", day="*", hour="0", minute=30)
+# scheduler.add_job(schedule_job, "interval", seconds=120)
 
 scheduler.start()
