0a2044ef2294a32d5d215b570e5e0c8e4d513001
Sajil Awale <sajil.awale@CGNPLAP271.local>
2022-10-09 21:13:16 +0545

    new

--- cronjob.py
+++ cronjob.py
@@ -6,11 +6,13 @@ from notify_new_listing import schedule_job
 from fundamental_analysis import funda_analysis_job
 
 # Create an instance of scheduler and add function.
-scheduler = BlockingScheduler(timezone="Asia/Kathmandu")
+scheduler1 = BlockingScheduler(timezone="Asia/Kathmandu")
+scheduler2 = BlockingScheduler(timezone="Asia/Kathmandu")
 
 
-scheduler.add_job(schedule_job, "cron", day="*", hour="7", minute=30)
-scheduler.add_job(funda_analysis_job, "cron", day="sat,sun", hour="7", minute=30)
+scheduler1.add_job(schedule_job, "cron", day="*", hour="7", minute=30)
+scheduler2.add_job(funda_analysis_job, "cron", day="sun", hour="7", minute=30)
 # scheduler.add_job(schedule_job, "interval", seconds=120)
 
-scheduler.start()
+scheduler1.start()
+scheduler2.start()
